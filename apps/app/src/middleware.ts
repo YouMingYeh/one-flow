b34a2c4ecb5b87960860fe6607271040a03205a8
YouMingYeh <ym911216@gmail.com>
2024-07-22 19:00:03 +0800

    chore: Update middleware config to exclude apple-touch-icon.png from routing

--- apps/app/src/middleware.ts
+++ apps/app/src/middleware.ts
@@ -106,6 +106,6 @@ export const config = {
      * - _next/image (image optimization files)
      */
 
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|apple-touch-icon.png).*)',
   ],
 };
