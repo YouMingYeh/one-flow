de7677482a2e653675f317d4ea2f605e0e1e31aa
YouMingYeh <b10705052@ntu.edu.tw>
2024-09-30 20:52:30 +0800

    Adjust email

--- apps/app/src/emails/email.tsx
+++ apps/app/src/emails/email.tsx
@@ -78,18 +78,53 @@ const Email = ({ username, id }: EmailProps) => (
         <Row>
           <Column align='center'>
             <Row>
-              <td align='center' className='w-1/2 pl-[16px]' colSpan={1}>
+              <td
+                align='center'
+                colSpan={1}
+                style={{ paddingLeft: 16, width: '50%' }}
+              >
                 <Button
-                  className='box-border w-full rounded-[8px] border border-solid border-gray-200 bg-white px-[20px] py-[12px] text-center font-semibold text-gray-900'
                   href='https://one-flow.cn'
+                  style={{
+                    width: '100%',
+                    boxSizing: 'border-box',
+                    paddingLeft: 20,
+                    paddingRight: 20,
+                    paddingTop: 12,
+                    paddingBottom: 12,
+                    borderRadius: 8,
+                    borderWidth: 1,
+                    borderStyle: 'solid',
+                    borderColor: 'rgb(229,231,235)',
+                    textAlign: 'center',
+                    backgroundColor: 'rgb(255,255,255)',
+                    fontWeight: 600,
+                    color: 'rgb(17,24,39)',
+                  }}
                 >
                   目前没有兴趣
                 </Button>
               </td>
-              <td align='center' className='w-1/2 pr-[16px]' colSpan={1}>
+              <td
+                align='center'
+                colSpan={1}
+                style={{ paddingRight: 16, width: '50%' }}
+              >
                 <Button
-                  className='box-border w-full rounded-[8px] bg-indigo-600 px-[20px] py-[12px] text-center font-semibold text-white'
                   href={`https://one-flow.cn/early-access/email/${id}`}
+                  style={{
+                    width: '100%',
+                    boxSizing: 'border-box',
+                    paddingLeft: 20,
+                    paddingRight: 20,
+                    paddingTop: 12,
+                    paddingBottom: 12,
+                    borderRadius: 8,
+                    backgroundColor: 'rgb(79,70,229)',
+                    textAlign: 'center',
+                    fontWeight: 600,
+                    color: 'rgb(255,255,255)',
+                  }}
                 >
                   立即成为内测用户
                 </Button>
